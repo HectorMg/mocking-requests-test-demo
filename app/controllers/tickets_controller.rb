6b056b8939263b8fa6e0abb0893cf2f9137c719f
Hector Mg <hectormgwake@gmail.com>
2016-08-23 21:33:43 -0500

    Adds tickets controller

---
+++ app/controllers/tickets_controller.rb
@@ -0,0 +1,23 @@
+class TicketsController < ApplicationController
+  def index
+    @tickets = Ticket.all
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def delete
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,17 @@
 Rails.application.routes.draw do
+  get 'tickets/index'
+
+  get 'tickets/show'
+
+  get 'tickets/new'
+
+  get 'tickets/create'
+
+  get 'tickets/edit'
+
+  get 'tickets/delete'
+
+  get 'tickets/update'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
